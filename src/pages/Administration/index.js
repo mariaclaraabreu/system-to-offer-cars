ff5e15b70e6072a18507577e42a42eb46991d1bc
Maria Clara <mclara.engsoftware@gmail.com>
2020-09-27 23:08:45 -0300

    building update administration page

--- src/pages/Administration/index.js
+++ src/pages/Administration/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-// import { Input, Button, List, Avatar } from 'antd'
 import Header from '../../components/Header';
 import Input from '../../components/Input';
 import firebase from '../../services/api'
@@ -11,6 +10,12 @@ import './styles.css';
 const Administration = () => {
   const [offers, setOffers] = useState([])
   const [filteredOffers, setFilteredOffers] = useState([])
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [id, setId] = useState('')
+  const [brand, setBrand] = useState('')
+  const [model, setModel] = useState('')
+  const [price, setPrice] = useState('')
+  const [year, setYear] = useState('')
 
   async function handleDeleteOffer(id) {
     firebase
@@ -28,16 +33,37 @@ const Administration = () => {
       })
   }
 
-  async function handleYetImplemented() {
-    alert('Ops! Sorry, but we havent implemented this functionality yet')
+  async function handleDataLoadingUpdate(id) {
+    setIsUpdate(true);
+    setOffers(offers.map((offer) => {
+      if (offer.id === id) {
+        setId(offer.id);
+        setBrand(offer.brand);
+        setModel(offer.model);
+        setPrice(offer.price);
+        setYear(offer.year);
+      }
+    }))
+  }
+
+  async function handleUpdateOffer(id) {
+    firebase
+      .firestore()  
+      .collection("offers")
+      .doc(id)
+      .update({
+        brand,
+        model,
+        price,
+        year
+    });
+
+    setIsUpdate(false);
   }
 
   async function handleSearchOffer(search) {
-    if (search.length === 0) {
-      setFilteredOffers(offers)
-    } else {
-      setFilteredOffers(offers.filter((offer) => offer.brand.includes(search)))
-    }
+    (search.length === 0) ? setFilteredOffers(offers)
+    : setFilteredOffers(offers.filter((offer) => offer.brand.includes(search)));
   }
 
   useEffect(() => {
@@ -66,39 +92,76 @@ const Administration = () => {
       <div className="container">
         <div className="content">
           <h1 className="title-adm">Adm</h1>
-          <small>{"Search"}</small>
-          <Input 
-            placeholder="Search for the offer by car brand name. Ex: Fiat"
-            onChange={function (e) {
-              handleSearchOffer(e.target.value)
-            }}
-          />
-          <ul className="list-items">
-            {filteredOffers.map(item => (
-              <OfferItem 
-                key={item.id}
-                brand={item.brand}
-                model={item.model}
-                onClickEdit={() => handleYetImplemented(item.id)}
-                onClickDelete={() => handleDeleteOffer(item.id)}
-              >
-              </OfferItem>
-            ))}
-          </ul>
+          {!isUpdate &&
+            <>
+              <small>{"Search"}</small>
+              <Input 
+                placeholder="Search for the offer by car brand name. Ex: Fiat"
+                onChange={function (e) {
+                  handleSearchOffer(e.target.value)
+                }}
+              />
+  
+              <ul className="list-items">
+                {filteredOffers.map(item => (
+                  <OfferItem 
+                    key={item.id}
+                    brand={item.brand}
+                    model={item.model}
+                    onClickEdit={() =>  handleDataLoadingUpdate(item.id)}
+                    onClickDelete={() => handleDeleteOffer(item.id)}
+                  >
+                  </OfferItem>
+                ))}
+              </ul>
+              <Link to='/newoffer'>
+                <Button
+                  name="Add New Offer"
+                  className="blue"
+                />
+              </Link>
+            </>
+          }
 
-          <Link to='/newoffer'>
-            <Button
-              name="Add New Offer"
-              className="blue"
-            />
-          </Link>
+          {isUpdate &&
+            <>
+              <h3>Update Offer</h3>
+              <form action="">
+                {"Brand"}
+                <Input
+                  placeholder="Ex: Fiat"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+                {"Model"}
+                <Input
+                  placeholder="Ex: Beetle"
+                  value={model}
+                  onChange={(e) => setModel(e.target.value)}
+                />
+                {"Price"}
+                <Input
+                  placeholder='In reais'
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+                {"Year"}
+                <Input
+                  placeholder='Ex: 2005'
+                  value={year}
+                  onChange={(e) => setYear(e.target.value)}
+                />
+              </form>
+              <Button
+                name="Save"
+                className="blue"
+                onClick={() => handleUpdateOffer(id)}
+              />
 
+            </>
+          }
         </div>
-      
-
       </div>
-
-      
     </>
   )
 }
